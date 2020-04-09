c4e910ad5d46bc5e5580af826d7ffc74d83333e7
Elysia Gabe <elysiagabe@gmail.com>
2020-04-09 09:38:45 -0700

    Add start script and dynamic port assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+// MAKE THE PORT DYNAMIC INSTEAD OF ASSIGNING VALUE (LIKE 5000)
+// ...allow Heroku to assign port when deploying
+const port = process.env.PORT || 5000; // process.env.PORT will be Heroku-assigned port, but if that's not available (e.g., in local dev environment), it will use port 5000
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
